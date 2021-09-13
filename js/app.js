b12c9a03d25058ec9018482ccdc4e2aed7b469d0
abrar-hossain <abrarmashuk@gmail.com>
2021-09-14 04:22:41 +0600

    function

--- js/app.js
+++ js/app.js
@@ -29,8 +29,8 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-  updateTotal();
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
